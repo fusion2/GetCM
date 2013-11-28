d43a0dee196dd0f7229bbb05792e3b97f8fa141e
cameron581 <cameron581@cox.net>
2013-11-27 18:37:29 -0600

    Update devicemap.py

--- getcm/model/devicemap.py
+++ getcm/model/devicemap.py
@@ -1,7 +1,3 @@
 devicemap = {
-    'blade2_p736e': 'ZTE Crescent (TMO)',
-    'blade2_p736v': 'ZTE Crescent (OUK)',
-    'u8815': 'Huawei Ascend G300',
-    'u8833': 'Huawei Ascend Y300',
-    'u8951': 'Huawei Ascend G510'
+    'u8665': 'Huawei Fusion 2',
 }
